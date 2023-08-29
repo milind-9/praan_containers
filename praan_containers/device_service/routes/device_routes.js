87c2e86d27a81b4ea2f9b16d512bb8cd9b80d335
milind-9 <143262139+milind-9@users.noreply.github.com>
2023-08-29 16:44:40 +0530

    Update device_routes.js

--- praan_containers/device_service/routes/device_routes.js
+++ praan_containers/device_service/routes/device_routes.js
@@ -1,16 +1,19 @@
 const express = require('express')
-
+const multer = require('multer');
 const { isAuthenticateUser,authorizeRole } = require('../middleware/auth')
 const router = express.Router()
 
-//router.route('/user').post(createUser)
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-router.route('/device').post(require('../controllers/add_device').createUser)
+router.route('/device').post(isAuthenticateUser,require('../controllers/add_device').createUser)
 
-router.route('/:device_id').get(require('../controllers/add_device').get_device)
+router.route('/:device_id').get(isAuthenticateUser,require('../controllers/add_device').get_device)
 
 router.route('').get(isAuthenticateUser,require('../controllers/add_device').get_all_device)
 
 
-router.route('/:device_id').patch(require('../controllers/add_device').updateDevice)
+router.route('/:device_id').patch(isAuthenticateUser,require('../controllers/add_device').updateDevice)
+
+router.route('/upload').post(upload.single('file'),isAuthenticateUser,require('../controllers/csv').upload_data)
 module.exports = router
